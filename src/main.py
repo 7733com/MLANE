f47020f31bdc7babff60e32a4da398e2c87ce104
Cui Chen <992129771@qq.com>
2020-01-04 17:29:42 +0800

    second commit

--- src/main.py
+++ src/main.py
@@ -6,13 +6,14 @@ if __name__ == "__main__":
     G=nx.read_edgelist('../data/citeseer/citeseer.edgelist',
                          create_using = nx.Graph(), nodetype = None, data = [('weight', int)])
     graph_label_path = '../data/citeseer/citeseer_labels.txt'
+
     '''
     initializing model
     '''
     model = MLANE(G, graph_label_path, walk_length=40, num_walks=10, window_size=10, embed_size=128, train_percent=0.8)
 
     '''
-    trainning policy
+    training policy
     '''
     model.policy_train(i_episode=5, epoch=50)
     model.save_model('citeseer')
